1c4ae2e3acbc71b6ca7bfd746cdbbda49ab8e784
Matheus Altrão <matheusaltrao2@gmail.com>
2024-07-09 14:17:27 -0400

    :sparkles: feat(components/ui): add authenticated and unauthenticated states to `Header` component

--- src/components/ui/header.tsx
+++ src/components/ui/header.tsx
@@ -1,12 +1,24 @@
 'use client';
-import { Menu } from 'lucide-react';
+import {
+  Sheet,
+  SheetContent,
+  SheetDescription,
+  SheetHeader,
+  SheetTitle,
+  SheetTrigger,
+} from '@/components/ui/sheet';
+import { Calendar, Home, LogOut, Menu } from 'lucide-react';
 import { signIn, signOut, useSession } from 'next-auth/react';
 import Image from 'next/image';
 import Logo from '../../assets/logos/main.svg';
+import ActiveLink from './active-link';
+import { Avatar, AvatarFallback, AvatarImage } from './avatar';
 import { Button } from './button';
+import Divider from './divider';
+import ServiceList from './service-list';
 
 const Header = () => {
-  const {} = useSession();
+  const { data, status } = useSession();
 
   const handleSignInClick = async () => {
     await signIn();
@@ -25,13 +37,97 @@ const Header = () => {
         src={Logo}
       />
 
-      <Button
-        variant={'outline'}
-        size={'sm'}
-      >
-        {' '}
-        <Menu size={20} />{' '}
-      </Button>
+      <Sheet>
+        <SheetTrigger asChild>
+          <Button
+            variant={'outline'}
+            size={'sm'}
+          >
+            <Menu size={20} />{' '}
+          </Button>
+        </SheetTrigger>
+        <SheetContent>
+          <SheetHeader>
+            <SheetTitle>Menu</SheetTitle>
+          </SheetHeader>
+
+          {status == 'authenticated' && (
+            <div className=" mt-8">
+              <div className="flex items-center gap-2">
+                <Avatar className="h-[44px] w-[44px]">
+                  <AvatarImage
+                    className={'rounded-full'}
+                    src={data?.user?.image!}
+                  />
+                  <AvatarFallback>{data?.user?.name}</AvatarFallback>
+                </Avatar>
+
+                <div>
+                  <h2 className="font-bold">{data?.user?.name}</h2>
+                  <p className="text-sm text-gray-400">{data?.user?.email}</p>
+                </div>
+              </div>
+            </div>
+          )}
+
+          {status == 'unauthenticated' && (
+            <div>
+              <SheetDescription>
+                <p>
+                  Você ainda não fez login, entre com o Google para liberar as novas
+                  funcionalidades.
+                </p>
+              </SheetDescription>
+
+              <Button
+                onClick={handleSignInClick}
+                className=" mt-8 w-full"
+              >
+                Entrar com Google
+              </Button>
+            </div>
+          )}
+
+          {status == 'authenticated' && (
+            <div>
+              <Divider />
+
+              <div className="flex flex-col gap-2">
+                <ActiveLink
+                  href="/"
+                  icon={<Home size={20} />}
+                  name="Início"
+                />
+
+                <ActiveLink
+                  href="/bookings"
+                  icon={<Calendar size={20} />}
+                  name="Agendamentos"
+                />
+              </div>
+            </div>
+          )}
+
+          {status == 'authenticated' && (
+            <div>
+              <Divider />
+
+              <ServiceList />
+
+              <Divider />
+
+              <Button
+                onClick={handleSignOutClick}
+                className="w-full gap-2 justify-start "
+                variant={'destructive'}
+              >
+                {' '}
+                <LogOut size={20} /> <span> Sair da conta</span>
+              </Button>
+            </div>
+          )}
+        </SheetContent>
+      </Sheet>
     </div>
   );
 };
