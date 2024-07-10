ecaef4ce072ee311c5f78c8a183044cc119f5d92
Matheus Altrão <matheusaltrao2@gmail.com>
2024-07-10 16:13:15 -0400

    :sparkles: feat(components/ui): Add `Loading` component with spinning animation

---
+++ src/components/ui/loading.tsx
@@ -0,0 +1,14 @@
+import { Loader2 } from 'lucide-react';
+
+const Loading = () => {
+  return (
+    <div>
+      <Loader2
+        className="animate-spin"
+        size={20}
+      />
+    </div>
+  );
+};
+
+export default Loading;
