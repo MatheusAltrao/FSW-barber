8d8cec962f164b4217c2b930b4b5f047c4a4a06d
Matheus Altrão <matheusaltrao2@gmail.com>
2024-07-10 16:05:06 -0400

    :sparkles: feat(actions/booking): Add `SaveBookingAction` to handle booking creation

---
+++ src/app/actions/booking/save-booking-action.ts
@@ -0,0 +1,21 @@
+'use server';
+
+import { db } from '@/app/lib/prisma';
+
+interface SaveBookingActionProps {
+  barbershopId: string;
+  serviceId: string;
+  userId: string;
+  date: Date;
+}
+
+export const SaveBookingAction = async (params: SaveBookingActionProps) => {
+  await db.booking.create({
+    data: {
+      serviceId: params.serviceId,
+      barbershopId: params.barbershopId,
+      userId: params.userId,
+      date: params.date,
+    },
+  });
+};
