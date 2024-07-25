160415aadfd479712496ce9bd20d030b4c91992e
Matheus Altrão <matheusaltrao2@gmail.com>
2024-07-25 15:09:46 -0400

    feat: add route barbershop with id

--- src/app/barbershops/[id]/page.tsx
+++ src/app/barbershops/[id]/page.tsx
@@ -37,11 +37,7 @@ const BarbersopDetailsPage = async ({ params }: BarbersopDetailsPageProps) => {
 
       <div className="px-5">
         <SectionTitle text="SOBRE NÓS" />
-        <p className="text-sm">
-          Bem-vindo à {barbershop.name}, onde tradição encontra estilo. Nossa equipe de mestres
-          barbeiros transforma cortes de cabelo e barbas em obras de arte. Em um ambiente acolhedor,
-          promovemos confiança, estilo e uma comunidade unida.
-        </p>
+        <p className="text-sm">Bem-vindo à {barbershop.name},</p>
       </div>
 
       <Divider />
