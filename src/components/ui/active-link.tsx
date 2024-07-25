d310c744a5c9b372207006948affa081909484e9
Matheus Altrão <matheusaltrao2@gmail.com>
2024-07-25 15:09:03 -0400

    fix: add verification in isActive using ===

--- src/components/ui/active-link.tsx
+++ src/components/ui/active-link.tsx
@@ -12,13 +12,13 @@ interface ActiveLinkProps {
 
 const ActiveLink = ({ href, name, icon }: ActiveLinkProps) => {
   const pathname = usePathname();
-  const activeLink = pathname.startsWith(href);
+  const isActive = href === '/' ? pathname === href : pathname.startsWith(href);
 
   return (
     <Link href={href}>
       <div
         className={`hover:opacity-80 rounded-md transition-opacity px-4 h-11 flex items-center gap-3 ${
-          activeLink ? 'bg-primary' : 'bg-transparent'
+          isActive ? 'bg-primary' : 'bg-transparent'
         }`}
       >
         {icon} <span>{name}</span>
