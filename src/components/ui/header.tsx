169d957b2414f6316fc7b4c91ca1cbaa82a42151
Matheus Altrão <matheusaltrao2@gmail.com>
2024-07-25 15:08:42 -0400

    feat: add link on logo to home

--- src/components/ui/header.tsx
+++ src/components/ui/header.tsx
@@ -10,6 +10,7 @@ import {
 import { Calendar, Home, LogOut, Menu } from 'lucide-react';
 import { signIn, signOut, useSession } from 'next-auth/react';
 import Image from 'next/image';
+import Link from 'next/link';
 import Logo from '../../assets/logos/main.svg';
 import ActiveLink from './active-link';
 import { Avatar, AvatarFallback, AvatarImage } from './avatar';
@@ -30,13 +31,14 @@ const Header = () => {
 
   return (
     <div className="h-20 flex items-center border-b justify-between px-5">
-      <Image
-        width={100}
-        height={20}
-        alt="FSW Barber"
-        src={Logo}
-      />
-
+      <Link href={'/'}>
+        <Image
+          width={100}
+          height={20}
+          alt="FSW Barber"
+          src={Logo}
+        />
+      </Link>
       <Sheet>
         <SheetTrigger asChild>
           <Button
