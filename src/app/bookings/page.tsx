966b5d4cd6225522df90323cddbbdca1372a3fcf
Matheus Altrão <matheusaltrao2@gmail.com>
2024-07-26 12:16:58 -0400

    feat: add show booking in home

--- src/app/bookings/page.tsx
+++ src/app/bookings/page.tsx
@@ -2,7 +2,6 @@ import BookingItem from '@/components/ui/booking-item';
 import Divider from '@/components/ui/divider';
 import Header from '@/components/ui/header';
 import SectionTitle from '@/components/ui/sectiont-title';
-import { isFuture, isPast } from 'date-fns';
 import { getServerSession } from 'next-auth';
 import { redirect } from 'next/navigation';
 import { authOptions } from '../api/auth/[...nextauth]/route';
@@ -16,18 +15,32 @@ const Bookings = async () => {
     return redirect('/');
   }
 
-  const bookings = await db.booking.findMany({
-    where: {
-      userId: (session.user as any).id,
-    },
-    include: {
-      service: true,
-      barbershop: true,
-    },
-  });
-
-  const confirmedBookings = bookings.filter((booking) => isFuture(booking.date));
-  const finishedBookings = bookings.filter((booking) => isPast(booking.date));
+  const [confirmedBookings, finishedBookings] = await Promise.all([
+    db.booking.findMany({
+      where: {
+        userId: (session.user as any).id,
+        date: {
+          gte: new Date(),
+        },
+      },
+      include: {
+        service: true,
+        barbershop: true,
+      },
+    }),
+    db.booking.findMany({
+      where: {
+        userId: (session.user as any).id,
+        date: {
+          lt: new Date(),
+        },
+      },
+      include: {
+        service: true,
+        barbershop: true,
+      },
+    }),
+  ]);
 
   return (
     <div>
@@ -38,31 +51,41 @@ const Bookings = async () => {
 
         <div className="space-y-8">
           <div className="space-y-5">
-            <SectionTitle text="Confimardos" />
+            {confirmedBookings.length > 0 && <SectionTitle text="Confimardos" />}
 
-            <div className="space-y-3">
-              {confirmedBookings.map((booking) => (
-                <BookingItem
-                  booking={booking}
-                  key={booking.id}
-                />
-              ))}
+            <div className="space-y-3 transition-all duration-200">
+              {/* adicionar framermotion */}
+              {confirmedBookings.length == 0 ? (
+                <p className="text-muted-foreground">Você não tem nenhum serviço agendado.</p>
+              ) : (
+                confirmedBookings.map((booking) => (
+                  <BookingItem
+                    booking={booking}
+                    key={booking.id}
+                  />
+                ))
+              )}
             </div>
           </div>
 
-          <Divider />
-          <div className="space-y-5">
-            <SectionTitle text="Finalizados" />
+          {finishedBookings.length > 0 && (
+            <div>
+              <Divider />
 
-            <div className="space-y-3 opacity-80">
-              {finishedBookings.map((booking) => (
-                <BookingItem
-                  booking={booking}
-                  key={booking.id}
-                />
-              ))}
+              <div className="space-y-5">
+                <SectionTitle text="Finalizados" />
+
+                <div className="space-y-3 opacity-80">
+                  {finishedBookings.map((booking) => (
+                    <BookingItem
+                      booking={booking}
+                      key={booking.id}
+                    />
+                  ))}
+                </div>
+              </div>
             </div>
-          </div>
+          )}
         </div>
       </div>
     </div>
